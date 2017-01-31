37907c37b1c1df403bbdb9ea01395e7a50b4eb24
Ubuntu <ubuntu@ip-10-0-0-175.ap-northeast-1.compute.internal>
2017-01-31 17:55:01 +0000

    Implement connection to RDS

--- ChamomileBatch/Package.swift
+++ ChamomileBatch/Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "ChamomileBatch"
+    name: "ChamomileBatch",
+    dependencies: [
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url:"https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0)
+    ]
 )
+
--- ChamomileBatch/Sources/main.swift
+++ ChamomileBatch/Sources/main.swift
@@ -1,1 +1,39 @@
+import Foundation
+import PerfectLib
+import MySQL
+import PerfectHTTP
+
+let env = ProcessInfo.processInfo.environment
+let testHost = env["DB_HOST"]
+let testUser = env["DB_USER"]
+let testPassword = env["DB_PASS"]
+let testPort = UInt32(env["DB_PORT"]!)!
+
+let testSchema = "nicovideo"
+
+let dataMysql = MySQL()
+
+func useMysql() {
+    guard dataMysql.connect(host: testHost, user: testUser, password: testPassword, port: testPort) else {
+        Log.info(message: "Failure connecting to data server \(testHost)")
+        return
+    }
+    defer {
+        dataMysql.close()
+    }
+    print("connected.")
+    guard dataMysql.selectDatabase(named: testSchema) && dataMysql.query(statement: "select * from users limit 1") else {
+        Log.info(message: "Failure: \(dataMysql.errorCode()) \(dataMysql.errorMessage())")
+        return
+    }
+    print("exec.")
+    let results = dataMysql.storeResults()
+
+    while let row = results?.next() {
+        print(row)
+    }
+}
+
 print("Hello, world!")
+useMysql()
+
